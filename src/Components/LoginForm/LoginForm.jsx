eb7fb515c669fc772f08298060e0eb299b6228f7
Claudia.Alvaro <claudiaalvaro17@gmail.com>
2024-05-23 10:11:38 +0200

    Update: login form styling and add icons

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './LoginForm.css'
+import { FaUser,FaLock } from "react-icons/fa";
+
+
 
 export const LoginForm = () => {
   return (
@@ -9,13 +12,11 @@ export const LoginForm = () => {
             <h1>Login</h1>
             <div className="input-box">
                 <input type="text" placeholder="Username" required />
+                <FaUser className='icon'/>
             </div>
             <div className="input-box">
                 <input type="password" placeholder="Password" required />
-            </div>
-            <div className="rembeber-forgot">
-                <label><input type="checkbox" /> Remember me</label>
-                <a href="#">Forgot password?</a>
+                <FaLock className='icon'/>
             </div>
             <button type="submit">Login</button>
             <div className="register-link">
